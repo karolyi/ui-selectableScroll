651690dfc6d6775c0e76856ce7a2324edf1a49ff
László Károlyi <laszlo@karolyi.hu>
2013-04-01 00:41:00 +0200

    Documentation

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ selectableScroll
 ================
 
 An extended jQuery-ui selectable with scroll capabilities
+
+# Documentation
+https://github.com/karolyi/ui-selectableScroll/wiki/How-does-it-work
--- selectableScroll.js
+++ selectableScroll.js
@@ -1,3 +1,23 @@
+/**
+ * This module is an extension to jQuery ui selectable, which scrolls when you try to select
+ * outside the container, and the scrollables are not fitting it originally.
+ *
+ * Created by László Károlyi <http://linkedin.com/in/karolyi>
+ * with the GPLv3 License: http://opensource.org/licenses/GPL-3.0
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 (function ($) {
   $.widget('ui.selectableScroll', $.ui.selectable, {
     options: {
